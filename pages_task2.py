4145e9a0a1f212b7cb66eb3eccd2b27480f7ee26
matveyvolodin <153534241+matveyvolodin@users.noreply.github.com>
2024-09-11 12:40:31 +0500

    test_navigating_using_the_paginator_number_button added

--- pages_task2.py
+++ pages_task2.py
@@ -1,5 +1,5 @@
 
-from selene import be, have, browser
+from selene import be, have, browser, query
 from selene.support.shared.jquery_style import s, ss
 
 # Urls
@@ -9,9 +9,11 @@ main_page_url = "https://makarovartem.github.io/frontend-avito-tech-test-assignm
 by_category_dropdown = s('(//span[@class="ant-select-selection-item"])[2]')
 genre_elements = ss('//div[contains(@class, "ant-typography css-17a39f8") and text()="Genre: "]')
 paginator_next_page_button = s('(//button[@class="ant-pagination-item-link"])[2]')
+paginator_number_button = s('(//li[@title=2])[1]')
 
 game_card_container = s('(//div[@class="ant-card-body"])[1]')
 back_to_main_button = s('//button[@type="button"]')
+page_game_titles = ss('//div[@class="_container_vlg32_23"]/div[1]/h1')
 
 
 def visit(url):
@@ -20,3 +22,10 @@ def visit(url):
 
 def select_dropdown_by_genre_option(option):
     s(f'//div[contains(@class, "ant-select-item") and text()="{option}"]').click()
+
+
+def create_game_titles_set():
+    page_game_titles.should(have.size_greater_than(0))
+    game_titles_set = {title.get(query.text) for title in page_game_titles}
+    return game_titles_set
+
--- test_task2.py
+++ test_task2.py
@@ -21,6 +21,7 @@ def test_filtering_by_category(genre, expected_actual_text):
     visit(main_page_url)
     by_category_dropdown.click()
     select_dropdown_by_genre_option(genre)
+    # Цикл проверки жанра после фильтрации по category по всем страницам пагинатора
     while True:
         genre_elements.should(have.size_greater_than(0))
         for element in genre_elements:
@@ -36,5 +37,19 @@ def test_redirecting_to_the_main_page_using_back_to_main_button():
     visit(main_page_url)
     game_card_container.should(be.clickable).click()
     back_to_main_button.should(be.clickable).click()
+    # Проверка фактического Url
     browser.should(have.url(main_page_url))
 
+
+@allure.title('TC_003_01 | Paginator > Navigating using the paginator number button')
+def test_navigating_using_the_paginator_number_button():
+    visit(main_page_url)
+    # Созданием множества с названиями игр с первой страницы пагинатора
+    titles_set_page1 = create_game_titles_set()
+    paginator_number_button.should(be.clickable).click()
+    # Проверка  2 кнопки пагинатора на "активность"
+    paginator_number_button.should(have.css_class('ant-pagination-item-active'))
+    # Созданием множества с названиями игр со второй страницы пагинатора
+    titles_set_page2 = create_game_titles_set()
+    # Проверка пересечения множеств (игры, отображенные на обеих страницах не повторяются)
+    assert not titles_set_page1.intersection(titles_set_page2)
